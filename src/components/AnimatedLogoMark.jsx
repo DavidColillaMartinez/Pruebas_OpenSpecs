dac93bf465c507b3e32ce339662b0c796c7b1cad
DavidColillaMartinez <davicete45@gmail.com>
2026-06-12 16:45:24 +0200

    Finalize premium animated logo reveal

--- src/components/AnimatedLogoMark.jsx
+++ src/components/AnimatedLogoMark.jsx
@@ -2,9 +2,6 @@ import { useId } from 'react';
 
 export function AnimatedLogoMark({ className = '' }) {
   const clipId = useId().replace(/:/g, '');
-  const outerClipId = `${clipId}-logo-outer`;
-  const innerClipId = `${clipId}-logo-inner`;
-  const lineClipId = `${clipId}-logo-line`;
   const thinFilterId = `${clipId}-logo-thin`;
 
   return (
@@ -16,16 +13,6 @@ export function AnimatedLogoMark({ className = '' }) {
         role="img"
       >
         <defs>
-          <clipPath id={outerClipId}>
-            <path d="M250 53 L350 252 L329 252 L250 96 L171 252 L150 252 Z" />
-          </clipPath>
-          <clipPath id={innerClipId}>
-            <path d="M250 143 L250 180 L246 190 L236 210 L226 230 L216 250 L206 270 L196 291 H132 L141 273 H186 L197 250 L207 230 L217 210 L227 190 Z" />
-            <path d="M250 143 L273 190 L283 210 L293 230 L303 250 L313 270 L324 291 H368 L359 273 H304 L294 270 L284 250 L274 230 L264 210 L254 190 L250 180 Z" />
-          </clipPath>
-          <clipPath id={lineClipId}>
-            <rect x="98" y="232" width="304" height="8" rx="1.5" />
-          </clipPath>
           <filter id={thinFilterId} x="-4%" y="-4%" width="108%" height="108%" colorInterpolationFilters="sRGB">
             <feMorphology in="SourceAlpha" operator="erode" radius="0.75" result="thinAlpha" />
             <feComposite in="SourceGraphic" in2="thinAlpha" operator="in" />
@@ -33,15 +20,16 @@ export function AnimatedLogoMark({ className = '' }) {
         </defs>
 
         <g className="logo-gold-line">
-          <image href="/logopng.png" width="500" height="306" preserveAspectRatio="none" clipPath={`url(#${lineClipId})`} filter={`url(#${thinFilterId})`} />
+          <rect x="98" y="233" width="304" height="6" rx="1.5" fill="#c1aa67" />
         </g>
 
-        <g className="logo-outer" clipPath={`url(#${outerClipId})`}>
-          <image href="/logopng.png" width="500" height="306" preserveAspectRatio="none" filter={`url(#${thinFilterId})`} />
+        <g className="logo-outer" filter={`url(#${thinFilterId})`}>
+          <path d="M250 53 L350 252 L329 252 L250 96 L171 252 L150 252 Z" fill="#050505" />
         </g>
 
-        <g className="logo-inner" clipPath={`url(#${innerClipId})`}>
-          <image href="/logopng.png" width="500" height="306" preserveAspectRatio="none" filter={`url(#${thinFilterId})`} />
+        <g className="logo-inner" filter={`url(#${thinFilterId})`}>
+          <path d="M250 143 L250 180 L246 190 L236 210 L226 230 L216 250 L206 270 L196 291 H132 L141 273 H186 L197 250 L207 230 L217 210 L227 190 Z" fill="#050505" />
+          <path d="M250 143 L273 190 L283 210 L293 230 L303 250 L313 270 L324 291 H368 L359 273 H304 L294 270 L284 250 L274 230 L264 210 L254 190 L250 180 Z" fill="#050505" />
         </g>
       </svg>
     </span>
