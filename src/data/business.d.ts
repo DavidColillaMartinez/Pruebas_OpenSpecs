0d4c3999325891c4b4ca5177f55e35d629e66322
DavidColillaMartinez <davicete45@gmail.com>
2026-09-09 14:02:03 +0200

    fix(assistant): include TypeScript support declarations

---
+++ src/data/business.d.ts
@@ -0,0 +1,6 @@
+export declare const PHONE: string;
+export declare const PHONE_INTL: string;
+export declare const ADDRESS: string;
+export declare const GOOGLE_BUSINESS_PROFILE_URL: string;
+export declare const MAPS_URL: string;
+export declare const INSTAGRAM_URL: string;
